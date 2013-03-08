8d49f0fd296200265c21af283075fcce5f43b750
Ryan Schmukler <ryan@womstreet.com>
2013-03-08 16:33:42 -0600

    Implemented sync.remove and sync.update

--- index.js
+++ index.js
@@ -11,6 +11,42 @@ module.exports = function(baseUrl) {
         cb(res.error, res.body);
       });
     };
+
+    sync.get = function(id, cb) {
+      var url = sync.baseUrl + '/' + id;
+      request.get(url, function(res) {
+        cb(res.error, res.body);
+      });
+    };
+
+    sync.removeAll = function(fn) {
+      var url = sync.baseUrl;
+      request.del(url, function(res) {
+        fn(res.error, res.body);
+      });
+    };
+
+    sync.save = function(fn) {
+      var url = sync.baseUrl;
+
+      request
+      .post(url)
+      .send(this.toJSON())
+      .end(function(res) {
+        fn(res.error, res.body);
+      });
+    };
+
+    sync.update = function(fn) {
+      var url = sync.baseUrl + "/" + this.primary();
+
+      request
+      .post(url)
+      .send(this.toJSON())
+      .end(function(res) {
+        fn(res.error, res.body);
+      });
+    };
   }
 
   return function(Model) {
--- test/ajax.js
+++ test/ajax.js
@@ -60,4 +60,214 @@ describe("Ajax Sync", function() {
       });
     });
   });
+
+  describe(".get()", function() {
+
+    it("does a GET request to the base url with the ID", function(done) {
+      var get = superagent.get;
+      superagent.get = function(url, cb) {
+        expect(url).to.be('/users/1');
+        cb({});
+      };
+
+      User.sync.get(1, function() {
+        superagent.get = get;
+        done();
+      });
+    });
+
+    it("passes along data from superagent", function(done) {
+      var get = superagent.get;
+      superagent.get = function(url, cb) {
+        cb({error: null, body: {id: "1", name: "Bob"}});
+      };
+
+      User.sync.get(1, function(err, body) {
+        expect(err).to.be(null);
+        expect(body).to.have.property('id', '1');
+        superagent.get = get;
+        done();
+      });
+    });
+
+    it("passes along errors from superagent", function(done) {
+      var get = superagent.get;
+      superagent.get = function(url, cb) {
+        cb({error: true, body: undefined});
+      };
+
+      User.sync.get(1, function(err, body) {
+        expect(err).to.be(true);
+        superagent.get = get;
+        done();
+      });
+    });
+  });
+
+  describe(".removeAll()", function() {
+    it("does a DELETE request to the base URL", function(done) {
+      var del = superagent.del;
+      superagent.del = function(url, cb) {
+        expect(url).to.be('/users');
+        cb({});
+      };
+      User.removeAll(function() {
+        superagent.del = del;
+        done();
+      });
+    });
+
+    it("forwards on errors from superagent", function(done) {
+      var del = superagent.del;
+      superagent.del = function(url, cb) {
+        expect(url).to.be('/users');
+        cb({error: true});
+      };
+      User.removeAll(function(error) {
+        expect(error).to.be(true);
+        superagent.del = del;
+        done();
+      });
+    });
+  });
+
+  describe(".save()", function() {
+    it("does a POST request to the base URL", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        expect(url).to.be('/users');
+        return {
+          send: function() {
+            return { end: function(cb) {cb({}); }};
+          }
+        };
+      };
+
+      var user = new User();
+      user.name('Bob');
+      user.save(function() {
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("POSTs the attributes of the model", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return {
+          send: function(data) {
+            expect(data).to.have.property('name', 'Bob');
+            return { end: function(cb) {cb({}); }};
+          }
+        };
+      };
+
+      var user = new User();
+      user.name('Bob');
+      user.save(function() {
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("passes along data from superagent", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return { send: function() { return { end: function(cb) {cb({body: {id: "513"}}); }}; } };
+      };
+
+      var user = new User();
+      user.name('Bob');
+      user.save(function(err) {
+        expect(user.id()).to.be("513");
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("passes along errors from superagent", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return { send: function() { return { end: function(cb) {cb({error: true}); }}; } };
+      };
+
+      var user = new User();
+      user.name('Bob');
+      user.save(function(err) {
+        expect(err).to.be(true);
+        superagent.post = post;
+        done();
+      });
+    });
+  });
+
+  describe(".update()", function() {
+    it("does a POST request to the base URL with the ID", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        expect(url).to.be('/users/1');
+        return {
+          send: function() {
+            return { end: function(cb) {cb({}); }};
+          }
+        };
+      };
+
+      var user = new User({id: "1"});
+      user.name('Bob');
+      user.save(function() {
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("POSTs the attributes of the model", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return {
+          send: function(data) {
+            expect(data).to.have.property('name', 'Bob');
+            return { end: function(cb) {cb({}); }};
+          }
+        };
+      };
+
+      var user = new User({id: "1"});
+      user.name('Bob');
+      user.save(function() {
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("passes along data from superagent", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return { send: function() { return { end: function(cb) {cb({body: {name: "Bobby"}}); }}; } };
+      };
+
+      var user = new User({id: "123"});
+      user.name('Bob');
+      user.save(function(err) {
+        expect(user.name()).to.be("Bobby");
+        superagent.post = post;
+        done();
+      });
+    });
+
+    it("passes along errors from superagent", function(done) {
+      var post = superagent.post;
+      superagent.post = function(url) {
+        return { send: function() { return { end: function(cb) {cb({error: true}); }}; } };
+      };
+
+      var user = new User({id: "123"});
+      user.name('Bob');
+      user.save(function(err) {
+        expect(err).to.be(true);
+        superagent.post = post;
+        done();
+      });
+    });
+  });
 });
